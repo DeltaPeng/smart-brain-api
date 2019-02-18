cb741ae756589e74ce872278633f466f0e44c8a5
DeltaPeng <32214601+DeltaPeng@users.noreply.github.com>
2019-02-17 22:08:10 -0700

    corrected var declaration

--- controllers/image.js
+++ controllers/image.js
@@ -3,20 +3,20 @@
 const Clarifai = require('clarifai');    
 
 const handleApiCall = (req, res, postgresDB) => {
-
-	const clariKey = "" 
+	//since it changes value, MUST be var
+	var clariKey = "" 
 	postgresDB('additional').select('data')
 	  .where('key', '=', 'clarifai')
 	  .returning('data')	
-	   .then(data => { 
-		clariKey = data[0].data.toString().trim(); 
-		console.log("test1: ",clariKey);
-		const app = new Clarifai.App({apiKey: clariKey});		 
+	   .then(data1 => { 
+			clariKey = data1[0].data.toString().trim(); 
+			console.log("test1: ",clariKey);
+			const app = new Clarifai.App({apiKey: clariKey});		 
 
-		app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.inputVal) 
-		.then(data2 => {
-			res.json(data2);
-		})
+			app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.inputVal) 
+			.then(data2 => {
+				res.json(data2);
+			})
 		.catch(err => res.status(400).json('unable to work with API'))
 	   }) 
 	   .catch (err => res.status(400).json('unable to get key'))    
